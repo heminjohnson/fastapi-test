9f974fd226a592ccbbe0585cbd0f1df25e4f1940
Hemin Johnson <achayan1992@gmail.com>
2024-11-19 23:46:13 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,6 +10,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Hemin"}
 
+
 @app.get("/ticker/{ticker}")
 def read_item(ticker: str, q: Optional[str] = None):
     return {"ticker": ticker, "q": q, "data": "hemin"}
+
+
+@app.get("/reddit/trend")
+async def reddit_trend():
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://tradestie.com/api/v1/apps/reddit")
+        return response.json()
