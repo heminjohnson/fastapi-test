460af752c654d0bf02114e9ac665e2f8d682e3e9
Hemin Johnson <achayan1992@gmail.com>
2024-11-20 00:08:41 +0100

    update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Add your frontend's URL here
+    allow_origins=["http://localhost:5173","https://pws-client.vercel.app"],  # Add your frontend's URL here
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
